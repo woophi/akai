fcc15abd5ed1bb87835846634dfc565bbaaec524
konst <konstantin.mikheev@icord.cz>
2019-08-02 08:49:07 +0200

    Logger

--- server/logger/logger.ts
+++ server/logger/logger.ts
@@ -1,5 +1,3 @@
-import config from '../config';
-
 type Logger = {
   info: (msg: any, ...args: any) => void;
   debug: (msg: any, ...args: any) => void;
@@ -8,20 +6,12 @@ type Logger = {
 
 export const Logger: Logger = {
   info: (msg, ...rest) => {
-    if (config.DEV_MODE) {
-      console.log('\x1b[36m%s\x1b[0m', msg, ...rest);
-    }
+    console.log('\x1b[36m%s\x1b[0m', msg, ...rest);
   },
   debug: (msg, ...rest) => {
-    if (config.DEV_MODE) {
-      console.debug('\x1b[33m%s\x1b[0m', msg, ...rest);
-    }
+    console.debug('\x1b[33m%s\x1b[0m', msg, ...rest);
   },
   error: (msg, ...rest) => {
-    if (config.DEV_MODE) {
-      console.trace('\x1b[41m%s\x1b[0m', 'ERROR ', msg, ...rest);
-    } else {
-      console.trace('\x1b[41m%s\x1b[0m', 'ERROR ', msg, ...rest);
-    }
+    console.trace('\x1b[41m%s\x1b[0m', 'ERROR ', msg, ...rest);
   }
 };
