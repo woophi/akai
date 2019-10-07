9146a4de302fa89333d2116a50d2de10ab029082
konst <konstantin.mikheev@icord.cz>
2019-10-07 16:15:59 +0200

    fq tracking

--- components/ui/molecules/shares/Shares.tsx
+++ components/ui/molecules/shares/Shares.tsx
@@ -5,7 +5,6 @@ import Icon from '@material-ui/core/Icon';
 import { makeStyles } from '@material-ui/core';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { useInterval } from 'core/lib';
-import axios from 'axios';
 
 let shareWindow: Window = null;
 
@@ -26,16 +25,6 @@ export const Shares = React.memo<Props>(({ linkToShare }) => {
     },
     processing ? 1000 : null
   );
-
-  React.useEffect(() => {
-    console.warn(
-      'dasd',
-      axios({
-        url: `https://vk.com/share.php?act=count&url=${encodeURIComponent(linkToShare)}`,
-        method: 'get'
-      })
-    );
-  }, [linkToShare]);
 
   const vkShare = React.useCallback(() => {
     setProcess(true);
