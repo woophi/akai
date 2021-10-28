5a8a8b0f585e27257c82ec24ce3703ddde02f914
woophi <mik.kon6610@gmail.com>
2021-10-28 16:48:46 +0200

    changed theme colors and spaces

--- components/ui/molecules/HeadIntro.tsx
+++ components/ui/molecules/HeadIntro.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, makeStyles } from '@material-ui/core';
+import { Box, Button, makeStyles, useMediaQuery } from '@material-ui/core';
 import MailIcon from '@material-ui/icons/Mail';
 import PhoneForwardedIcon from '@material-ui/icons/PhoneForwarded';
 import PlaceIcon from '@material-ui/icons/Place';
@@ -6,10 +6,12 @@ import { memo } from 'react';
 import { SocialButtons } from 'ui/atoms';
 
 export const HeadIntro = memo(() => {
-  const classes = useStyles({});
+  const isSmallEnough = useMediaQuery('(max-width:800px)');
+  const classes = useStyles({ isSmallEnough });
+  const margin = isSmallEnough ? '0 1rem' : '0.25rem .5rem';
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" flexWrap="wrap" className={classes.content}>
-      <Box display="flex" justifyContent="center" alignItems="center" margin="0.25rem .5rem" color="#fff" marginLeft="auto">
+    <Box display="flex" alignItems="center" flexWrap="wrap" className={classes.content}>
+      <Box display="flex" justifyContent="center" alignItems="center" margin={margin} color="#fff">
         <Button
           startIcon={<PlaceIcon />}
           href="https://goo.gl/maps/EKJ6expGRbri8wC39"
@@ -20,17 +22,17 @@ export const HeadIntro = memo(() => {
           Karlův most, 110 00 Praha 1
         </Button>
       </Box>
-      <Box display="flex" justifyContent="center" alignItems="center" margin="0.25rem .5rem" color="#fff">
+      <Box display="flex" justifyContent="center" alignItems="center" margin={margin} color="#fff">
         <Button startIcon={<PhoneForwardedIcon />} href="tel:+420776561613" color="inherit" size="small">
           +420 776 561 613
         </Button>
       </Box>
-      <Box display="flex" justifyContent="center" alignItems="center" margin="0.25rem .5rem" color="#fff">
+      <Box display="flex" justifyContent="center" alignItems="center" margin={margin} color="#fff">
         <Button startIcon={<MailIcon />} href="mailto:akaidoart@gmail.com" color="inherit" size="small">
           akaidoart@gmail.com
         </Button>
       </Box>
-      <Box display="flex" justifyContent="center" alignItems="center" marginX="auto">
+      <Box display="flex" justifyContent="center" alignItems="center" margin={margin}>
         <SocialButtons size="s" />
       </Box>
     </Box>
@@ -38,7 +40,9 @@ export const HeadIntro = memo(() => {
 });
 
 const useStyles = makeStyles(theme => ({
-  content: {
+  content: ({ isSmallEnough }: { isSmallEnough: boolean }) => ({
     backgroundColor: '#353535',
-  },
+    paddingLeft: isSmallEnough ? '' : '4rem',
+    justifyContent: isSmallEnough ? 'center' : undefined,
+  }),
 }));
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,16 +10,10 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           {/* Use minimum-scale=1 to enable GPU rasterization */}
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-          />
+          <meta name="theme-color" content="#353535" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
           <link
             rel="stylesheet"
             href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
